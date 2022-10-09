d5383eb2508f0a86752984bd87ee45bfb8dab9c4
nakamura0907 <nakamura990907@gmail.com>
2022-10-09 21:13:48 +0900

    テスト環境の作成

---
+++ services/app/src/core/domain/value/BookScore.ts
@@ -0,0 +1,23 @@
+import Exception from "@/lib/Exception";
+
+type Score = 0 | 1 | 2 | 3 | 4 | 5;
+
+class BookScore {
+    static readonly MIN_SCORE = 0;
+    static readonly MAX_SCORE = 5;
+
+    constructor(private readonly _value: Score) {}
+
+    get value() {
+        return this._value;
+    }
+
+    static validate(value: number) {
+        if (!Number.isInteger(value)) throw new Exception("不正なスコアです");
+        if (value < this.MIN_SCORE || value > this.MAX_SCORE) throw new Exception(`書籍のスコアは${this.MIN_SCORE}から${this.MAX_SCORE}までの数値です`);
+
+        return new BookScore(value as Score);
+    }
+}
+
+export default BookScore;
