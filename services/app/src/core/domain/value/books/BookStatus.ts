ba3e6274bfd7ef2819810fb5a0a595adf469c685
nakamura0907 <nakamura990907@gmail.com>
2022-10-09 22:49:32 +0900

    読書状態クラス作成

---
+++ services/app/src/core/domain/value/books/BookStatus.ts
@@ -0,0 +1,31 @@
+import Exception from "@/lib/Exception";
+
+// 読書状態
+class BookStatus {
+    private readonly _value: number;
+
+    static validate(value: number) {
+        if (value === 0) return new BookStatus(value);
+        if (value === 1) return new BookStatus(value);
+        if (value === 2) return new BookStatus(value);
+        if (value === 3) return new BookStatus(value);
+        throw new Exception("不正な読書状態です", 400);
+    }
+
+    constructor(value: number) {
+        this._value = value;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    get label() {
+        if (this._value === 1) return "読みたい";
+        if (this._value === 2) return "いま読んでる";
+        if (this._value === 3) return "読み終わった";
+        return "未設定";
+    }
+}
+
+export default BookStatus
